e68ac90296233b356c3a4c388190d5214cae2285
Simon Long <simon@raspberrypi.com>
2025-01-17 16:37:52 +0000

    Correct copyright

--- src/updater.c
+++ src/updater.c
@@ -40,7 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <packagekit-glib2/packagekit.h>
 
 /*----------------------------------------------------------------------------*/
-/* Typedefs and macros */
+/* Typedefs and macros                                                        */
 /*----------------------------------------------------------------------------*/
 
 #define DEBUG_ON
